a9d7f6fe9914d4f665739c3ac69a525fe7a2c099
maria-S-456 <mars381pika@gmail.com>
2017-05-13 02:23:49 -0400

    embedded js with google map

--- README.txt
+++ README.txt
@@ -1,8 +1,1 @@
-Redid parking app using EJS view engine, routes, and with most forms on a single page except login. Working authentication using local database. Map, contact, suggestion, sign up, and login forms work correctly.
-
-update: accessing public api through a sample route to test that it's working. data appearing in console to ensure it's being accessed.
-
-update: can display list of parking houses in console correctly.
-
-update: Getting coordinates of location entered by user
-		Correct format now.
+update: preparing to separate google map code into separate file.
--- src/views/parkDataScript.js
+++ src/views/parkDataScript.js
@@ -1,8 +1,58 @@
+/*
 var parkingUrl = 'https://api.parkwhiz.com/search/?lat=41.8857256&lng=-87.6369590&start=1490681894&end=1490692694&key=62d882d8cfe5680004fa849286b6ce20';
 $.getJSON(parkingUrl, function(url){
 
 	for(var i = 0; i < 20; i++){
 		console.log(url.parking_listings[i].location_name);
 	}
-	
+
 });
+*/
+
+
+   	function initMap() {
+     
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 15,
+          center: {lat: 41.8781, lng: -87.6298}
+        });
+
+         var geocoder = new google.maps.Geocoder();
+        document.getElementById('submit').addEventListener('click', function() {          
+          geocodeAddress(geocoder, map);
+        });
+      }
+    function geocodeAddress(geocoder, resultsMap)
+        {
+          var address = document.getElementById('address').value;
+          geocoder.geocode({'address':address}, function(results, status){
+            if(status === 'OK'){
+              resultsMap.setCenter(results[0].geometry.location);
+
+              var marker = new google.maps.Marker({
+                map: resultsMap,
+                position: results[0].geometry.location
+              });
+              var x = results[0].geometry.location;
+              var latitude = JSON.parse(JSON.stringify(x)).lat;
+              var longitude = JSON.parse(JSON.stringify(x)).lng;
+              console.log('longitude: ' + longitude + '. latitude: ' + latitude);
+
+
+              var parkingUrl = 'https://api.parkwhiz.com/search/?lat=41.8857256&lng=-87.6369590&start=1490681894&end=1490692694&key=62d882d8cfe5680004fa849286b6ce20';
+              $.getJSON(parkingUrl, function(url){
+                for(var i = 0; i < 20; i++){
+                console.log(url.parking_listings[i].location_name);}
+              });
+            } else{
+              alert('Geocode was not successful for the following reason: ' + status);
+            }
+          });
+        }
+
+function runMapData(){
+	initMap();
+	geocodeAddress();
+}
+
+module.exports = runMapData();
