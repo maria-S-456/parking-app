341a056cd8b76ca0d6f0cde4c1b2695dc9baabf3
maria <mars381pika@gmail.com>
2017-07-30 10:23:24 -0400

    removed authconfig from .gitignore

---
+++ src/config/authconfig.js
@@ -0,0 +1,9 @@
+//Your own username and password would be specified here in order to make the nodemailer contact form work. pass is 'password' for privacy.
+module.exports = {
+	mailer: {
+		auth: {
+			user: 'mars381pika@gmail.com',
+			pass: 'password'
+		}
+	}
+}
